cb707b107ac6a61ef910067223aac8232bac8094
Sopan <sopan.mittal@weaveability.com>
2019-04-10 16:37:11 +0530

    bug fixes and add exp for sqof

--- example.js
+++ example.js
@@ -4,3 +4,8 @@ console.log("Calling sqrtOf for 3", numberz.sqrtOf(3));
 console.log("Calling sqrtOf for String", numberz.sqrtOf('wertyuiuy'));
 console.log("Calling sqrtOf for String Number 9", numberz.sqrtOf('9'));
 console.log("Calling sqrtOf for -3", numberz.sqrtOf(-3));
+console.log();
+console.log("Calling sqOf for 3", numberz.sqOf(3));
+console.log("Calling sqOf for String", numberz.sqOf('wertyuiuy'));
+console.log("Calling sqOf for String Number 9", numberz.sqOf('9'));
+console.log("Calling sqOf for -3", numberz.sqOf(-3));
--- index.js
+++ index.js
@@ -11,34 +11,49 @@ function sqrtOf(number) {
 
 function sqOf(number) {
     let result = {};
-    let powOfAnswer = powOf(number, 2);
-    result.answer = powOfAnswer.answer;
+    if (isNaN(number)) {
+        result.error = "Input Should be a Number";
+    } else {
+        let powOfAnswer = powOf(number, 2);
+        result.answer = powOfAnswer.answer;
+    }
     return result;
 }
 
 function twoPower(number) {
     let result = {};
-    let powOfAnswer = powOf(2, number);
-    result.answer = powOfAnswer.answer;
+    if (isNaN(number) ) {
+        result.error = "Input Should be a Number";
+    } else {
+        let powOfAnswer = powOf(2, number);
+        result.answer = powOfAnswer.answer;
+    }
     return result;
 }
 
 function tenPower(number) {
     let result = {};
-    let powOfAnswer = powOf(10, number);
-    result.answer = powOfAnswer.answer;
+    if (isNaN(number) ) {
+        result.error = "Input Should be a Number";
+    } else {
+        let powOfAnswer = powOf(10, number);
+        result.answer = powOfAnswer.answer;
+    }
     return result;
 }
 
 function powOf(base, power) {
     let result = {};
-    result.answer = Math.pow(base, power);
+    if (isNaN(base) || isNaN(power)) {
+        result.error = "Input Should be a Positive Number";
+    } else {
+        result.answer = Math.pow(base, power);
+    }
     return result;
-
 }
 
 module.exports.sqrtOf = sqrtOf;
-module.exports.sqOf;
-module.exports.twoPower;
-module.exports.tenPower;
-module.exports.powOf;
+module.exports.sqOf = sqOf;
+module.exports.twoPower = twoPower;
+module.exports.tenPower = tenPower;
+module.exports.powOf = powOf;
